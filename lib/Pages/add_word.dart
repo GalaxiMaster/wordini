2ffbc81bd64effe6acae2ad32fba625a2ba47c0a
Dylan <dmj08bot@gmail.com>
2025-05-09 19:43:42 +1000

    added word information with default dictionary api

--- lib/Pages/add_word.dart
+++ lib/Pages/add_word.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:free_dictionary_api_v2/free_dictionary_api_v2.dart';
 import 'package:vocab_app/file_handling.dart';
 
 class AddWord extends StatefulWidget {
@@ -44,9 +45,47 @@ class _AddWordState extends State<AddWord> {
   }
   
 }
-void addWordToList(String value) {
-  readData().then((data) {
-    data[value] = {'Definition': 'Test'};
+void addWordToList(String word) {
+  readData().then((data) async{
+    Map wordDetails = await getWordDef(word);
+    data[word] = wordDetails;
     writeData(data, append: false);
   });
+}
+
+Future<Map> getWordDef(String word) async {
+  try {
+    Map wordDetails = {
+      'word': word,
+      'definitions': []// [{
+      //   'definition': '',
+      //   'example': '',
+      //   'partOfSpeech': '',
+      //   'synonyms': [],
+      //   'antonyms': [],
+      // }],
+    };
+    // TODO probably switch to a different API, this ones not it
+    final dictionary = FreeDictionaryApiV2();
+    final response = await dictionary.getDefinition(word);
+    debugPrint(response.toString());
+    response[0].meanings?.forEach((meaning) {
+      meaning.definitions?.forEach((definition) {
+        Map meaningDetails = {
+          'partOfSpeech': meaning.partOfSpeech,
+          'definition': definition.definition,
+          'example': definition.example,
+          'synonyms': definition.synonyms,
+          'antonyms': definition.antonyms,
+        };
+
+        wordDetails['definitions'].add(meaningDetails);
+      });
+    });
+    return wordDetails;
+  } on FreeDictionaryException catch (error, stackTrace) {
+    debugPrint(error.toString());
+    return {};
+  }
+
 }
